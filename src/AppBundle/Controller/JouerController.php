3fcaff616ec8d1247a7bec00fe60f21ab3e12eb5
David Annebicque <david.annebicque@gmail.com>
2017-10-04 09:31:39 +0200

    Création du controller jouer

--- src/AppBundle/Controller/JouerController.php
+++ src/AppBundle/Controller/JouerController.php
@@ -6,6 +6,7 @@ use AppBundle\Entity\Partie;
 use AppBundle\Form\PartieType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class JouerController
@@ -17,11 +18,18 @@ class JouerController extends Controller
     /**
      * @Route("/nouvelle-partie")
      */
-    public function nouvellePartieAction()
+    public function nouvellePartieAction(Request $request)
     {
         $partie = new Partie();
         $form = $this->createForm(PartieType::class, $partie);
 
+        if ($request->isMethod('POST'))
+        {
+            // traitement du formulaire
+            // ajout des joueurs à la partie
+            // initialisation des données de la partie
+            // redirection vers la distribution des cartes
+        }
 
         return $this->render(':JouerController:nouvelle_partie.html.twig', array(
             'form' => $form->createView()
@@ -33,7 +41,26 @@ class JouerController extends Controller
      */
     public function distribuerCartesAction()
     {
+        //récupérer l'ensemble des cartes
+        //mélnager les cartes
+        //distribuer les cartes aux joueurs
+        //créer la pioche
+        //rediriger vers l'affichage du plateau
+
+
         return $this->render(':JouerController:distribuer_cartes.html.twig', array(
+            // ...
+        ));
+    }
+
+    /**
+     * @Route("/afficher")
+     */
+    public function afficherPlateauAction()
+    {
+        // Afficher le plateau
+
+        return $this->render(':JouerController:afficher_plateau.html.twig', array(
             // ...
         ));
     }
@@ -58,14 +85,6 @@ class JouerController extends Controller
         ));
     }
 
-    /**
-     * @Route("/afficher")
-     */
-    public function afficherPlateauAction()
-    {
-        return $this->render(':JouerController:afficher_plateau.html.twig', array(
-            // ...
-        ));
-    }
+
 
 }
