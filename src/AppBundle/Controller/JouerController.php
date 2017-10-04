951aaa6c73abe41a0ee43856e30e91e14f76e9c7
David Annebicque <david.annebicque@gmail.com>
2017-10-04 10:06:17 +0200

    Création du controller jouer

--- src/AppBundle/Controller/JouerController.php
+++ src/AppBundle/Controller/JouerController.php
@@ -7,6 +7,7 @@ use AppBundle\Form\PartieType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Encoder\JsonEncode;
 
 /**
  * Class JouerController
@@ -23,33 +24,70 @@ class JouerController extends Controller
         $partie = new Partie();
         $form = $this->createForm(PartieType::class, $partie);
 
-        if ($request->isMethod('POST'))
+        $form->handleRequest($request); //synchronisation des données du formulaire avec l'objet $partie via le formType
+        if ($form->isSubmitted() && $form->isValid())
         {
-            // traitement du formulaire
-            // ajout des joueurs à la partie
+            //récupére la connexion à la BDD
+            $em = $this->getDoctrine()->getManager();
+
             // initialisation des données de la partie
+
+            //récupération de toutes les bornes
+            $bornes = $em->getRepository("AppBundle:Borne")->findAll();
+
+            $tborne=array(); //tableau qui sera sauvegardé dans la BDD
+            $ordre = 1; //ordre des bornes
+            foreach ($bornes as $borne)
+            {
+                $tborne[$ordre] = array('id_borne' => $borne->getId(),
+                                        'position' => 'neutre');
+                $ordre ++;
+            }
+            //sauvegarde la liste des bornes dans ma partie
+            $partie->setListeDesBornes($tborne);
+
+            $cartes = $em->getRepository('AppBundle:Carte')->findAll();
+            $tcarte = array();
+            foreach ($cartes as $carte)
+            {
+                $tcarte[] = $carte->getId(); //sauvegarde les id des cartes dans un tableau
+            }
+
+            shuffle($tcarte); //mélange du tableau
+
+            //distribution de la main de J1
+            $mainJ1=array();
+            for($i = 0; $i<6; $i++)
+            {
+                $mainJ1[] = $tcarte[$i];
+            }
+            $partie->setMainj1($mainJ1);
+
+            //distributoon de la main de J2
+            $mainJ2=array();
+            for($i = 6; $i<12; $i++)
+            {
+                $mainJ2[] = $tcarte[$i];
+            }
+            $partie->setMainj2($mainJ2);
+
+            $pioche=array();
+            for($i = 12; $i < count($tcarte); $i++)
+            {
+                $pioche[] = $tcarte[$i];
+            }
+            $partie->setPioche($pioche);
+
+            $partie->setTourJoueur($partie->getJoueur1());
+
+            $em->persist($partie);
+            $em->flush();
+
             // redirection vers la distribution des cartes
         }
 
         return $this->render(':JouerController:nouvelle_partie.html.twig', array(
             'form' => $form->createView()
-        ));
-    }
-
-    /**
-     * @Route("/distribuer-cartes")
-     */
-    public function distribuerCartesAction()
-    {
-        //récupérer l'ensemble des cartes
-        //mélnager les cartes
-        //distribuer les cartes aux joueurs
-        //créer la pioche
-        //rediriger vers l'affichage du plateau
-
-
-        return $this->render(':JouerController:distribuer_cartes.html.twig', array(
-            // ...
         ));
     }
 
